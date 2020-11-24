d0ff96eeb911ec4823ee18c799851d8549fd89b1
Mark Karpeles <magicaltux@gmail.com>
2020-11-24 14:41:38 +0900

    move CFLAGS/LDFLAGS to config.go

---
+++ opus/config.go
@@ -0,0 +1,7 @@
+package opus
+
+/*
+#cgo CFLAGS: -DHAVE_CONFIG_H -D_FORTIFY_SOURCE=2 -DPIC -fvisibility=hidden -fstack-protector-strong -fno-common
+#cgo LDFLAGS: -lm
+*/
+import "C"
--- opus/dec.go
+++ opus/dec.go
@@ -23,8 +23,6 @@
 package opus
 
 /*
-#cgo CFLAGS: -DHAVE_CONFIG_H -D_FORTIFY_SOURCE=2 -DPIC -fvisibility=hidden -fstack-protector-strong -fno-common
-#cgo LDFLAGS: -lm
 #include "opus.h"
 
 typedef struct {
