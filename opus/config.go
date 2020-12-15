1dafdadc562ebd1802b35bec914d3409d8cdd518
Mark Karpeles <magicaltux@gmail.com>
2020-12-15 10:58:35 -0800

    add libssp on windows to have stack protector working

--- opus/config.go
+++ opus/config.go
@@ -3,6 +3,7 @@ package opus
 /*
 // standard compilation flags
 #cgo CFLAGS: -DHAVE_CONFIG_H -D_FORTIFY_SOURCE=2 -DPIC -fvisibility=hidden -fstack-protector-strong -fno-common
+#cgo windows LDFLAGS: -lssp
 
 // add includes needed for opus, including building the actual opus lib
 #cgo CFLAGS: -Iopus-1.3.1 -Iopus-1.3.1/include -Iopus-1.3.1/celt -Iopus-1.3.1/silk -Iopus-1.3.1/silk/float
